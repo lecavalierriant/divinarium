e849964a2bbd2ff926d6c6d7b46b905067587235
𝔄𝔯𝔱𝔲𝔰 𝔡𝔢 ℭ𝔥𝔞𝔳𝔞𝔤𝔫𝔞𝔠 <lecavalierriant@gmail.com>
2025-04-24 18:58:03 +0200

    Update Divinarium.js

--- Divinarium.js
+++ Divinarium.js
@@ -8,9 +8,27 @@ function initialiser() {
 	document.getElementById("select-initiales").addEventListener(
 		"change",
 		function() {
-			for (imago of document.getElementsByClassName("img-initiale")) {
-				source = imago.src.split("/");
-				imago.src = source[source.length - 3] + "/" + this.value + "/" + source[source.length - 1];
+			for (initiale of document.getElementsByClassName("img-initiale")) {
+				source = initiale.src.split("/");
+				initiale.src = source[source.length - 3] + "/" + this.value.toLowerCase() + "/" + source[source.length - 1];
+			}
+		}
+	);
+	document.getElementById("select-margines").addEventListener(
+		"change",
+		function() {
+			for (margine of document.getElementsByClassName("div-exterior")) {
+				source = margine.style.backgroundImage;
+				console.log("soucre = " + source);
+				margine.src = source[source.length - 3] + "/" + this.value.toLowerCase() + "/" + source[source.length - 1];
+			}
+		}
+	);
+	document.getElementById("input-imagines").addEventListener(
+		"change",
+		function() {
+			for (imago of document.getElementsByClassName("img-illustrationis")) {
+				imago.style.display = (imago.style.display == "none") ? "block" : "none";
 			}
 		}
 	);
