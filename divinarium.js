dd051b0e469ba0888ec9f91ba4e60c236cb2679d
𝔏𝔢 ℭ𝔞𝔳𝔞𝔩𝔦𝔢𝔯 ℜ𝔦𝔞𝔫𝔱 <lecavalierriant@gmail.com>
2026-09-05 01:28:12 +0200

    Update divinarium.js

--- divinarium.js
+++ divinarium.js
@@ -5,3 +5,21 @@ function initialiser() {
 }
 
 function charger() {localStorage.setItem("hauteur", window.scrollY);}
+
+window.addEventListener(
+	"load",
+	(event) => {
+		for (bbb of document.querySelectorAll("span")) {
+			bbb.addEventListener('click', croix);
+		}
+	}
+);
+
+index = 0;
+function croix() {
+	liste = ["♰", "☩", "✢", "✣", "✤", "✥", "🕂", "᛭", "✠"];
+	for (aaa of document.querySelectorAll("span")) {
+		aaa.innerText = liste[index];
+	}
+	if (index > liste.length - 2) {index = 0;} else {index++;}
+}
